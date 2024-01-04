35808291e08a669445ae3ce54fa836510a2b79a4
P R <23jan.priyanshuranjan@gmail.com>
2024-01-04 17:52:55 +0530

    Asian Time

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,5 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("First");
+        System.out.println("Postjira1");
+        System.out.println("Postjira2");
     }
 }
