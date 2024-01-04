1f5efba5e9aed35fd7ce26b5bbb0c335e38a880c
P R <23jan.priyanshuranjan@gmail.com>
2024-01-04 18:21:23 +0530

    American Developer

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -7,5 +7,8 @@ public class Test1 {
         System.out.println("Psatjira3");
         System.out.println("Changed by demo develop");
         System.out.println("Changed by demo develop2");
+
+        System.out.println("Changed by master develop");
+        System.out.println("Changed by master develop2");
     }
 }
